9e502b158b33d29edc7f74c76b69a3ccb8220115
Saarah <saarah.iq@hotmail.com>
2023-06-05 19:51:00 +1200

    inital commit

---
+++ AdvancedTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvancedTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
