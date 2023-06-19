a3c45f206ce5233176906408b89b81a65c6a974f
Saarah <saarah.iq@hotmail.com>
2023-06-19 15:12:33 +1200

    setting up folders in project

---
+++ AdvancedTask/JSON_Objects/SkillObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTask.JSON_Objects
+{
+    public class SkillObject
+    {
+        public string skill { get; set; }
+        public string skillLevel { get; set; }
+    }
+    
+}
+
